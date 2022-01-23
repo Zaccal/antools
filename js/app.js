6d15c44e1dde63553001619cd6373e7a17f37e73
Zaccal <ksss90411@gmail.com>
2022-01-23 15:43:48 +0600

    add header

---
+++ js/app.js
@@ -0,0 +1,24 @@
+new WOW().init()
+
+$(document).ready(function() {
+})
+
+const arrow = document.querySelector('.menu__link_fall span')
+
+const burger = document.querySelector('.burger')
+const menu = document.querySelector('.menu')
+const body = document.querySelector('body')
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active')
+    menu.classList.toggle('active')
+    body.classList.toggle('lock')
+})
+
+const arrowFall = document.querySelector('.arrow-fall')
+const innerMenu = document.querySelector('.menu__inner-list')
+
+arrowFall.addEventListener('click', () => {
+    arrowFall.classList.toggle('active')
+    innerMenu.classList.toggle('active')
+})
